650c1bfcda7ea70df80cc21cad1b3c240055b961
bassamkhalid9803 <bassam9803@gmail.com>
2025-06-30 18:46:01 +0500

    Select and validate a starting point(from)

--- cypress/e2e/user_journey.cy.js
+++ cypress/e2e/user_journey.cy.js
@@ -1,7 +1,26 @@
-describe('User Journey', () => {
-  it('should complete a basic user journey', () => {
-    // Add your user journey steps here
-    cy.visit('https://www.arrivabus.co.uk/  ');
-    // Example: cy.get('selector').click();
-  });
-}); 
+describe('Arriva Bus - Homepage', () => {
+  beforeEach(() => {
+    cy.visit('https://www.arrivabus.co.uk/')
+   cy.get('#onetrust-accept-btn-handler').click()
+    Cypress.on('uncaught:exception', (err, runnable) => {
+ 
+  return false;
+});
+  })
+  //The following IT has been edited by Bassam Khalid
+  it('Plan a journey - Select and validate a starting point(from)', () => {
+    cy.wait(6000)
+    cy.get('.search-select__value-container').eq(0).click()
+   
+  cy.get('[aria-label="Where are you coming from?"]').eq(0).type('L',{force:true})
+  cy.get('#react-select-3-option-0').should('not.exist')
+   cy.get('[aria-label="Where are you coming from?"]').eq(0).type('o',{force:true})
+  cy.get('#react-select-3-option-0').should('not.exist')
+   cy.get('[aria-label="Where are you coming from?"]').eq(0).type('n',{force:true})
+   cy.wait(5000)
+  cy.get('.search-select__menu-list').should('exist')
+  
+
+  })
+
+})
