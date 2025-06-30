cb7ab3898463f6e986a1651571f8e99df694b203
umar551869 <umar632708@gmail.com>
2025-06-30 18:47:48 +0500

    Calender

--- cypress/e2e/user_journey.cy.js
+++ cypress/e2e/user_journey.cy.js
@@ -1,7 +1,22 @@
+//Muhammad Umar Ilyas
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
+
 describe('User Journey', () => {
-  it('should complete a basic user journey', () => {
-    // Add your user journey steps here
-    cy.visit('https://www.arrivabus.co.uk/  ');
-    // Example: cy.get('selector').click();
-  });
+
+   beforeEach('Visits the site and accepts cookies', () => {
+    cy.clearCookies();
+    cy.visit('https://www.arrivabus.co.uk/');
+    cy.get('#onetrust-accept-btn-handler').click({force:true});
+  })
+it('Varifies Calender Date Selection', () => {
+  cy.get('#DPdefault').click({force:true})
+  cy.get('.react-datepicker__day.react-datepicker__day--030.react-datepicker__day--selected.react-datepicker__day--today').should('contain','30').click();
+  //cy.get('._2nLcqhYfJi2Jep-CiHF4mi > ._nfmPejYIgKBBPi4ONjx7').click({force:true}) 
+  cy.get('._2nLcqhYfJi2Jep-CiHF4mi').children().eq(2).click({force:true}) 
+
+})
+
+
 }); 
