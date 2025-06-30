ec2324a6499d7aa00124c61a1d366e2cfa42fd76
shahmeeriqbal-source <shahmeer.malik25@gmail.com>
2025-06-30 15:41:14 -0700

    First Commit

--- cypress/e2e/user_journey.cy.js
+++ cypress/e2e/user_journey.cy.js
@@ -1,7 +1,7 @@
 describe('User Journey', () => {
   it('should complete a basic user journey', () => {
     // Add your user journey steps here
-    cy.visit('/');
+    cy.visit('https://www.arrivabus.co.uk/  ');
     // Example: cy.get('selector').click();
   });
 }); 
