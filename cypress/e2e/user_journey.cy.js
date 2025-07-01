e801f2371375e0cf02c81c305588899a3f618fb4
umar551869 <umar632708@gmail.com>
2025-07-01 12:01:07 +0500

    calender1.2

--- cypress/e2e/user_journey.cy.js
+++ cypress/e2e/user_journey.cy.js
@@ -1,7 +1,4 @@
 //Muhammad Umar Ilyas
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
 
 describe('User Journey', () => {
 
@@ -10,13 +7,13 @@ describe('User Journey', () => {
     cy.visit('https://www.arrivabus.co.uk/');
     cy.get('#onetrust-accept-btn-handler').click({force:true});
   })
+
+//Muhammad Umar Ilyas
 it('Varifies Calender Date Selection', () => {
   cy.get('#DPdefault').click({force:true})
-  cy.get('.react-datepicker__day.react-datepicker__day--030.react-datepicker__day--selected.react-datepicker__day--today').should('contain','30').click();
-  //cy.get('._2nLcqhYfJi2Jep-CiHF4mi > ._nfmPejYIgKBBPi4ONjx7').click({force:true}) 
-  cy.get('._2nLcqhYfJi2Jep-CiHF4mi').children().eq(2).click({force:true}) 
-
+  cy.get('.react-datepicker__day.react-datepicker__day--001.react-datepicker__day--selected.react-datepicker__day--today').should('contain','1').click();
+  cy.get('._2nLcqhYfJi2Jep-CiHF4mi > ._nfmPejYIgKBBPi4ONjx7').click({force:true}) 
+  //cy.get('._2nLcqhYfJi2Jep-CiHF4mi').children().eq(1).click({force:true}) 
 })
-
 
 }); 
