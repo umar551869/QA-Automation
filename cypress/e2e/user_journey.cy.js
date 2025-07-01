431989ac4047735f36322dddb04ec615a22d81d5
tareenity <tareenityltd@gmail.com>
2025-07-01 12:29:49 +0500

    Semi-colon- Bassam

--- cypress/e2e/user_journey.cy.js
+++ cypress/e2e/user_journey.cy.js
@@ -19,10 +19,13 @@ describe('Arriva Bus - Homepage', () => {
    cy.get('[aria-label="Where are you coming from?"]').eq(0).type('n',{force:true})
    cy.wait(5000)
   cy.get('.search-select__menu-list').should('exist')
-  
-  });
 
-    it('should complete a basic user journey', () => {
+  
+
+  })
+
+    //shahmeer To function 
+it('should complete a basic user journey', () => {
     cy.wait(3000);
 
     
@@ -44,16 +47,5 @@ describe('Arriva Bus - Homepage', () => {
     
 
 });
-
-
-
-  it('Varifies Calender Date Selection', () => {
-  cy.get('#DPdefault').click({force:true})
-  cy.get('.react-datepicker__day.react-datepicker__day--030.react-datepicker__day--selected.react-datepicker__day--today').should('contain','30').click();
-  //cy.get('._2nLcqhYfJi2Jep-CiHF4mi > ._nfmPejYIgKBBPi4ONjx7').click({force:true}) 
-  cy.get('._2nLcqhYfJi2Jep-CiHF4mi').children().eq(2).click({force:true}) 
-
-    })
-
   
 })
